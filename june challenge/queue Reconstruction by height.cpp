41a38238eb71640114b304851f7eb04367c92c4e
harshitgrover12@gmail.com <harshitgrover12@gmail.com>
2020-06-07 02:42:12 +0530

    day 6 june challenge good ques

---
+++ june challenge/queue Reconstruction by height.cpp
@@ -0,0 +1,24 @@
+  bool cmp(vector<int>a,vector<int>b)
+    {
+        
+           if(a[0]==b[0])
+            return a[1]>b[1];
+        return a[0]<b[0];
+    }
+class Solution {
+public:
+  
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) 
+    {  
+      sort(people.begin(),people.end(),cmp);
+        
+        vector<vector<int> > p;
+        
+       for(int i=people.size()-1;i>=0;i--)
+       {
+            p.insert(p.begin()+people[i][1],people[i]);
+       }
+        
+        return p;     
+    }
+};
